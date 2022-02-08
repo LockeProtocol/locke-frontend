7f50706c1c2d0cc8c0fed011c2b2bb30ca637fcd
0xdef1 <0xdef1.eth@gmail.com>
2022-02-08 08:33:19 -0500

    Show correct network name win Wrong Network dialog

--- src/lib/utils/config.ts
+++ src/lib/utils/config.ts
@@ -1,5 +1,6 @@
 export default  {
     chainId: 99,
+    networkName: 'Locke Testnet',
     factory: '0x46b54058092eD7Bceb7c0538220f20265B0bd839',
     lens: '0x08c0B0227D33E79117EAF7c6F05B4435dA1159e2'
 }
